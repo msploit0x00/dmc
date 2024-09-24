15fa439d5ce9dbf610381097f2f38e977c634e4c
GrandMasterOfTemple <100480646+msploit0x00@users.noreply.github.com>
2024-09-24 17:39:38 +0300

    fixing issues

--- dmc/projected_qty.py
+++ dmc/projected_qty.py
@@ -4,14 +4,16 @@ import frappe
 
 
 
-
 @frappe.whitelist()
 def get_projected_qty(item_code):
-
     tot_avail_qty = frappe.db.sql(
-				"select projected_qty from `tabBin` \
-				where item_code = %s and warehouse = 'All Warehouses - D'",
-				(item_code),
-			)
+        "SELECT projected_qty FROM `tabBin` WHERE item_code = %s AND warehouse = 'All Warehouses - D'",
+        (item_code,),  
+        as_dict=False  
+    )
 
-    return tot_avail_qty
+    if tot_avail_qty:
+        return tot_avail_qty[0][0]  # Return the first projected_qty value if it exists
+    else:
+        return 0  # Return 0 if no record is found
+
