0360c1741ced785f037f8e691e73474c72d761bf
GrandMasterOfTemple <100480646+msploit0x00@users.noreply.github.com>
2024-10-10 15:30:07 +0300

    remaining function override on_submit

---
+++ dmc/remaining.py
@@ -0,0 +1,36 @@
+import frappe
+from erpnext.selling.doctype.sales_order.sales_order import SalesOrder
+
+
+
+class CustomSalesOrder(SalesOrder):
+    def on_submit(self):
+       so_items = self.items
+       supply_order = self.items[0].custom_supply_order
+
+       sup_order_doc = frappe.get_doc("Supply order", supply_order)
+       
+       sup_items = sup_order_doc.items
+
+
+
+       for row in so_items:
+        for orginal in sup_items:
+            if row.item_code == original.item_code:
+                orginal.cutom_remaining = orginal.custom_remaining - orginal.custom_total_sub_qty
+
+
+
+
+
+
+
+
+
+
+        # doctype: 'Quotation Item',
+        #                 name: org.name ,
+        #                 fieldname: 'custom_remaining',
+        #                 # value: org.custom_remaining - org.custom_total_sub_qty
+        
+        
