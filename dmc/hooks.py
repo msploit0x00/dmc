6e17098a8bec446848d7ad8f587af93b8cf03ea5
GrandMasterOfTemple <100480646+msploit0x00@users.noreply.github.com>
2024-10-10 17:07:49 +0300

    remving the overrride

--- dmc/hooks.py
+++ dmc/hooks.py
@@ -114,9 +114,9 @@ app_license = "mit"
 # ---------------
 # Override standard doctype classes
 
-override_doctype_class = {
-	"Sales Order": "dmc.remaining.CustomSalesOrder"
-}
+# override_doctype_class = {
+# 	"Sales Order": "dmc.remaining.CustomSalesOrder"
+# }
 
 # Document Events
 # ---------------
--- dmc/remaining.py
+++ dmc/remaining.py
@@ -1,20 +1,20 @@
 import frappe
-from erpnext.selling.doctype.sales_order.sales_order import SalesOrder
+# from erpnext.selling.doctype.sales_order.sales_order import SalesOrder
 
 
-class CustomSalesOrder(SalesOrder):
-    def on_submit(self):
-        so_items = self.items
-        supply_order = self.items[0].custom_supply_order
+# class CustomSalesOrder(SalesOrder):
+#     def on_submit(self):
+#         so_items = self.items
+#         supply_order = self.items[0].custom_supply_order
 
-        sup_order_doc = frappe.get_doc("Supply order", supply_order)  
+#         sup_order_doc = frappe.get_doc("Supply order", supply_order)  
 
-        sup_items = sup_order_doc.items
+#         sup_items = sup_order_doc.items
 
-        for row in so_items:
-            for original in sup_items:
-                if row.item_code == original.item_code:
-                    original.custom_remaining = original.qty - original.custom_total_sub_qty
+#         for row in so_items:
+#             for original in sup_items:
+#                 if row.item_code == original.item_code:
+#                     original.custom_remaining = original.qty - original.custom_total_sub_qty
 
 
 
