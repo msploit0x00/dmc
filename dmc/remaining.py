859a5a1021f56ba66d8ace0cf2d58b7d25c8e25d
GrandMasterOfTemple <100480646+msploit0x00@users.noreply.github.com>
2024-10-10 16:35:48 +0300

    set_remaining values

--- dmc/remaining.py
+++ dmc/remaining.py
@@ -3,11 +3,11 @@ from erpnext.selling.doctype.sales_order.sales_order import SalesOrder
 
 
 class CustomSalesOrder(SalesOrder):
-    def on_update_after_submit(self):
+    def on_submit(self):
         so_items = self.items
         supply_order = self.items[0].custom_supply_order
 
-        sup_order_doc = frappe.get_doc("Supply order", supply_order)  # Ensure correct casing
+        sup_order_doc = frappe.get_doc("Supply order", supply_order)  
 
         sup_items = sup_order_doc.items
 
@@ -16,8 +16,28 @@ class CustomSalesOrder(SalesOrder):
                 if row.item_code == original.item_code:
                     original.custom_remaining = original.qty - original.custom_total_sub_qty
 
-        sup_order_doc.save()  # Save the changes
-        frappe.db.commit()  # Commit the changes to the database
+
+
+
+
+
+
+@frappe.whitelist(allow_guest=True)
+def set_remaining(rem,row_name):
+    
+    update = frappe.db.sql("""
+
+        UPDATE `tabQuotation Item`
+        SET custom_remaining = %s
+        where name = %s
+    
+    
+    
+    
+    """,(rem,row_name))
+    frappe.db.commit()
+
+    return "Updated Successfully"
 
 
 
