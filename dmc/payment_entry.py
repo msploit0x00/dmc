e676bf93869a0ada5139dcd3acb58092da79610e
GrandMasterOfTemple <100480646+msploit0x00@users.noreply.github.com>
2024-10-16 11:02:24 +0300

    append reference payment

--- dmc/payment_entry.py
+++ dmc/payment_entry.py
@@ -8,13 +8,30 @@ class CustomPaymentEntry(PaymentEntry):
 
         proforma = self.custom_proforma_invoice_details
 
+        ref = self.custom_reference__payment_
+
+
+
         for row in proforma:
             prof = frappe.get_doc("Proforma Invoice", row.proforma_invoice)
             prof.ignore_validate_update_after_submit = True
             prof.db_set('collection_amount', row.to_be_paid)
 
+            self.append("custom_reference__payment_",{
+            'proforma_invoice': row.proforma_invoice,
+            'grand_total': row.grand_total,
+            'to_be_paid': row.to_be_paid,
+            'outstanding_amount': row.outstanding_amount
+            })
+
             if prof.collection_amount == prof.grand_total:
                 prof.db_set("fully_paid", 1)
+
+
+        self.set("custom_proforma_invoice_details", [])
+
+ 
+
 
         frappe.msgprint("Proforma Updated Successfully")
 
