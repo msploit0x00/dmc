300e360ac917e7199566d147309d0da056835c63
GrandMasterOfTemple <100480646+msploit0x00@users.noreply.github.com>
2024-12-24 13:44:56 +0200

    adding batch in child table

--- dmc/public/js/stock_reconcilition.override.js
+++ dmc/public/js/stock_reconcilition.override.js
@@ -40,62 +40,65 @@
 //   },
 // });
 frappe.ui.form.on("Stock Reconciliation", {
-    custom_barcode(frm) {
-      // Guard against infinite loop by checking if the event has already been processed
-      if (frm.custom_barcode_processed) {
-        return; // If the flag is set, exit the function
-      }
-  
-      frm.custom_barcode_processed = true; // Set the flag to true to indicate processing
-  
-      frappe.call({
-        method: "dmc.stock_reconcilition_override.getConv_factor_for_uom",
-        args: {
-          barcode: frm.doc.custom_barcode,
-          items: frm.doc.items,
-          doc: frm.doc,
-        },
-        callback: function (res) {
-          console.log("response", res.message);
-  
-          // Reset the flag after the processing is done
-          frm.custom_barcode_processed = false;
-  
-          // Check if response contains the necessary fields
-          if (res.message) {
-            let existing_item_codes = frm.doc.items.map(item => item.item_code);
-            let item_added = false;
-            
-            console.log("res.message.item_code", res.message.item_code);
-            console.log("res.message.conversion_factor", res.message.conversion_factor);
-            
-            // Loop through existing items to check if item exists
-            for (let item of frm.doc.items) {
-              if (item.item_code === res.message.item_code) {
-                // If the item exists, increment the quantity by the conversion factor
-                item.qty += res.message.conversion_factor;
-                item_added = true;
-                break; // No need to add a new row, just update the existing one
-              }
+  custom_barcode(frm) {
+    // Guard against infinite loop by checking if the event has already been processed
+    if (frm.custom_barcode_processed) {
+      return; // If the flag is set, exit the function
+    }
+
+    frm.custom_barcode_processed = true; // Set the flag to true to indicate processing
+
+    frappe.call({
+      method: "dmc.stock_reconcilition_override.getConv_factor_for_uom",
+      args: {
+        barcode: frm.doc.custom_barcode,
+        items: frm.doc.items,
+        doc: frm.doc,
+      },
+      callback: function (res) {
+        console.log("response", res.message);
+
+        // Reset the flag after the processing is done
+        frm.custom_barcode_processed = false;
+
+        // Check if response contains the necessary fields
+        if (res.message) {
+          let existing_item_codes = frm.doc.items.map(item => item.item_code);
+          let item_added = false;
+          console.log("res.message.barcode", res.message.barcode);
+          console.log("res.message.item_code", res.message.item_code);
+          console.log("res.message.conversion_factor", res.message.conversion_factor);
+          console.log("res.message.batch_no", res.message.batch_no);
+          // Loop through existing items to check if item exists
+          for (let item of frm.doc.items) {
+            if (item.item_code === res.message.item_code) {
+              // If the item exists, increment the quantity by the conversion factor
+              item.qty += res.message.conversion_factor;
+              item_added = true;
+              break; // No need to add a new row, just update the existing one
             }
-  
-            // If the item was not found in the existing items, add a new row
-            if (!item_added) {
-              let child = frm.add_child('items',{
-                "item_code":res.message.item_code,
-                "qty":res.message.conversion_factor,
-              });
-              // child.item_code = res.message.item_code;  // Set item_code
-              // child.qty = res.message.conversion_factor; // Set qty as conversion_factor
-              // child.conversion_factor = res.message.conversion_factor; // Set conversion_factor
-            }
-  
-            frm.refresh_field("items");
-            frm.set_value('custom_barcode', '');
-          } else {
-            return
           }
-        },
-      });
-    },
-  });
+
+          // If the item was not found in the existing items, add a new row
+          if (!item_added) {
+            let child = frm.add_child('items',{
+              "barcode":res.message.barcode,
+              "item_code":res.message.item_code,
+              "qty":res.message.conversion_factor,
+              "use_serial_batch_fields":1,  
+              "batch_no":res.message.batch_id,
+            });
+            // child.item_code = res.message.item_code;  // Set item_code
+            // child.qty = res.message.conversion_factor; // Set qty as conversion_factor
+            // child.conversion_factor = res.message.conversion_factor; // Set conversion_factor
+          }
+
+          frm.refresh_field("items");
+          frm.set_value('custom_barcode', '');
+        } else {
+          return
+        }
+      },
+    });
+  },
+});
--- dmc/stock_reconcilition_override.py
+++ dmc/stock_reconcilition_override.py
@@ -9,12 +9,13 @@ def getConv_factor_for_uom(barcode, items, doc):
     
     # Fetch barcode details
     data = get_barcode_details(barcode)
-    
+    print("=====================>",data)
     if not data:
         frappe.msgprint(f"Barcode {barcode} not found in barcode details.")
         return
     
     conversion_factor = data.get('conversion_factor')[0].get('conversion_factor')
     item_code = data.get('item_code')[0].get('parent')
-    return {"item_code":item_code,"conversion_factor": conversion_factor}
+    batch_id = data.get('batch_id')
+    return {"barcode":barcode,"item_code":item_code,"conversion_factor": conversion_factor,"batch_id":batch_id}
   
