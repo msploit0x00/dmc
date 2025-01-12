d54592f9f19243aec221c084254e433e9007c735
GrandMasterOfTemple <100480646+msploit0x00@users.noreply.github.com>
2025-01-12 14:06:11 +0200

    on_submit override

---
+++ dmc/permission_override.py
@@ -0,0 +1,12 @@
+import frappe
+
+
+from hr_sum_additionals.hr_sum_additionals.doctype.permission.permission import Permission
+from hr_sum_additionals.hr_sum_additionals.doctype.penalties_rules.penalties_rules import get_the_rule
+
+
+
+class CustomPermission(self):
+    def on_submit(self):
+        if self.docstatus == 1:
+            get_the_rule(employee_id=self.employee,date=self.date,doctype=self.doctype,ref_docname=self.name)
