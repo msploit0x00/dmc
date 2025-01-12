e1fdcdd7e50b0987c324d4b910f86070d38bf0ac
GrandMasterOfTemple <100480646+msploit0x00@users.noreply.github.com>
2025-01-12 14:07:07 +0200

    permission override

--- dmc/hooks.py
+++ dmc/hooks.py
@@ -125,6 +125,7 @@ doctype_js = {
 override_doctype_class = {
     "Payment Entry": "dmc.payment_entry.CustomPaymentEntry",
     "Material Request": "dmc.material_request.CustomPurchaseRequest",
+    "Permission": "dmc.permission_override.CustomPermission"
 }
 
 # Document Events
