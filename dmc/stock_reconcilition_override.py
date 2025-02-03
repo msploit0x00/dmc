3a61581cd01caf47523b25343589a74fe56aa57c
Mustafa Nazier <mustafanazieer@gmail.com>
2025-02-03 10:30:07 +0200

    Fixing the error exception in the stock_reconcilition_override.py file

--- dmc/stock_reconcilition_override.py
+++ dmc/stock_reconcilition_override.py
@@ -7,18 +7,18 @@ def getConv_factor_for_uom(barcode, items, doc):
     if not barcode:
         return
     
-    # Fetch barcode details
-    data = get_barcode_details(barcode)
-    print("=====================>",data)
-    if not data:
-        frappe.msgprint(f"Barcode {barcode} not found in barcode details.")
-        return
-    
-    if "error" in data:
-        frappe.msgprint(data["error"])
-        return
-        
     try:
+        # Fetch barcode details
+        data = get_barcode_details(barcode)
+        print("=====================>",data)
+        if not data:
+            frappe.msgprint(f"Barcode {barcode} not found in barcode details.")
+            return
+        
+        if "error" in data:
+            frappe.msgprint(data["error"])
+            return
+            
         conversion_factor = data.get('conversion_factor')
         item_code = data.get('item_code')[0].get('parent') if data.get('item_code') else None
         batch_id = data.get('batch_id')
