5599b8199eea77172932a898fc84e1e5c87069da
MinaAshraf67 <minaaashraff670@gmail.com>
2025-05-29 15:43:19 +0300

    workflow_report update

--- dmc/dmc/report/workflow_approval_summary/workflow_approval_summary.js
+++ dmc/dmc/report/workflow_approval_summary/workflow_approval_summary.js
@@ -12,17 +12,17 @@ frappe.query_reports["Workflow Approval Summary"] = {
 			label: "Workflow State",
 			fieldtype: "Data"
 		},
-		{
-			fieldname: "from_date",
-			label: "From Date",
-			fieldtype: "Date",
-			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1)
-		},
-		{
-			fieldname: "to_date",
-			label: "To Date",
-			fieldtype: "Date",
-			default: frappe.datetime.get_today()
-		}
+		// {
+		// 	fieldname: "from_date",
+		// 	label: "From Date",
+		// 	fieldtype: "Date",
+		// 	default: frappe.datetime.add_months(frappe.datetime.get_today(), -1)
+		// },
+		// {
+		// 	fieldname: "to_date",
+		// 	label: "To Date",
+		// 	fieldtype: "Date",
+		// 	default: frappe.datetime.get_today()
+		// }
 	]
 }
