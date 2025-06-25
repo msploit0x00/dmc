763136efe99020310eb27e143b01fb0150d515f0
MinaAshraf67 <minaaashraff670@gmail.com>
2025-06-25 16:39:38 +0300

    skipping valdaiton of over limit crossed

--- dmc/monkey_patches.py
+++ dmc/monkey_patches.py
@@ -1,3 +1,6 @@
+import erpnext.controllers.status_updater
+
+
 def no_validate_actual_qty(self, sn_doc):
     # Disabled all core validations in this method as per user request
     pass
@@ -6,3 +9,11 @@ def no_validate_actual_qty(self, sn_doc):
 def apply_monkey_patches():
     from erpnext.stock.serial_batch_bundle import SerialBatchBundle
     SerialBatchBundle.validate_actual_qty = no_validate_actual_qty
+
+
+def no_op_limits_crossed_error(self, args, item, qty_or_amount):
+    # Bypass the over limit validation (do nothing)
+    pass
+
+
+erpnext.controllers.status_updater.StatusUpdater.limits_crossed_error = no_op_limits_crossed_error
