3520b5b4749748801e21779c847015b82f61d291
MinaAshraf67 <minaaashraff670@gmail.com>
2025-09-02 21:44:08 +0300

    meter line dieminssion

--- dmc/dmc/doctype/meter_line/meter_line.py
+++ dmc/dmc/doctype/meter_line/meter_line.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class Lines(Document):
+class MeterLine(Document):
 	pass
--- dmc/dmc/doctype/meter_line/test_meter_line.py
+++ dmc/dmc/doctype/meter_line/test_meter_line.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestLines(FrappeTestCase):
+class TestMeterLine(FrappeTestCase):
 	pass
