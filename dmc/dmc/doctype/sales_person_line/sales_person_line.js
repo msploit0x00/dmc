228fa7afb7ad24b6522a0690c952ab672f04d619
MinaAshraf67 <minaaashraff670@gmail.com>
2025-09-03 17:25:31 +0300

    changing meter line to sales person line

--- dmc/dmc/doctype/sales_person_line/sales_person_line.js
+++ dmc/dmc/doctype/sales_person_line/sales_person_line.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, mina and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Meter Line", {
+// frappe.ui.form.on("Sales Person Line", {
 // 	refresh(frm) {
 
 // 	},
