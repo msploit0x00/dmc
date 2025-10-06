7c3eb257cd95f1ac5a5759d65961ef5afb029edd
MinaAshraf67 <minaaashraff670@gmail.com>
2025-10-06 22:20:23 +0300

    stock ledger warehouse pivot view

--- dmc/dmc/report/custom_stock_ledger/custom_stock_ledger.py
+++ dmc/dmc/report/custom_stock_ledger/custom_stock_ledger.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # License: GNU General Public License v3. See license.txt
 
-
 import copy
 from collections import defaultdict
 
@@ -21,6 +20,11 @@ from erpnext.stock.utils import (
 
 
 def execute(filters=None):
+    # أول حاجة: لو عايز pivot view
+    if filters and filters.get("show_pivot_view"):
+        return get_pivot_data(filters)
+
+    # الكود العادي يكمل
     is_reposting_item_valuation_in_progress()
     include_uom = filters.get("include_uom")
     columns = get_columns(filters)
@@ -61,43 +65,234 @@ def execute(filters=None):
 
     for sle in sl_entries:
         item_detail = item_details[sle.item_code]
+
         sle.update(item_detail)
         if bundle_info := bundle_details.get(sle.serial_and_batch_bundle):
             data.extend(get_segregated_bundle_entries(
                 sle, bundle_info, batch_balance_dict, filters))
             continue
+
         if filters.get("batch_no") or inventory_dimension_filters_applied:
             actual_qty += flt(sle.actual_qty, precision)
             stock_value += sle.stock_value_difference
             if sle.batch_no:
                 if not batch_balance_dict.get(sle.batch_no):
                     batch_balance_dict[sle.batch_no] = [0, 0]
+
                 batch_balance_dict[sle.batch_no][0] += sle.actual_qty
+
             if filters.get("segregate_serial_batch_bundle"):
                 actual_qty = batch_balance_dict[sle.batch_no][0]
+
             if sle.voucher_type == "Stock Reconciliation" and not sle.actual_qty:
                 actual_qty = sle.qty_after_transaction
                 stock_value = sle.stock_value
+
             sle.update({"qty_after_transaction": actual_qty,
                        "stock_value": stock_value})
-        # Custom override for Purchase Receipt
-        if sle.voucher_type == "Purchase Receipt":
-            sle["stock_value_difference"] = flt(
-                sle["actual_qty"]) * flt(sle["incoming_rate"])
-            sle["in_out_rate"] = sle["valuation_rate"]
-        else:
-            if sle.actual_qty:
-                sle["in_out_rate"] = flt(
-                    sle.stock_value_difference / sle.actual_qty, precision)
-            elif sle.voucher_type == "Stock Reconciliation":
-                sle["in_out_rate"] = sle.valuation_rate
+
+        sle.update({"in_qty": max(sle.actual_qty, 0),
+                   "out_qty": min(sle.actual_qty, 0)})
+
+        if sle.serial_no:
+            update_available_serial_nos(available_serial_nos, sle)
+
+        if sle.actual_qty:
+            sle["in_out_rate"] = flt(
+                sle.stock_value_difference / sle.actual_qty, precision)
+
+        elif sle.voucher_type == "Stock Reconciliation":
+            sle["in_out_rate"] = sle.valuation_rate
+
         data.append(sle)
+
         if include_uom:
             conversion_factors.append(item_detail.conversion_factor)
 
     update_included_uom_in_report(
         columns, data, include_uom, conversion_factors)
     return columns, data
+
+
+def get_pivot_data(filters=None):
+    """
+    تحويل الـ warehouse من صفوف لأعمدة
+    باقي الأعمدة تظهر عادي زي الـ original
+    """
+    # نجيب الـ data العادية
+    items = get_items(filters)
+    sl_entries = get_stock_ledger_entries(filters, items)
+
+    if not sl_entries:
+        return [], []
+
+    item_details = get_item_details(items, sl_entries, None)
+
+    # نجمع كل الـ warehouses الموجودة
+    warehouses = set()
+    for sle in sl_entries:
+        if sle.get('warehouse'):
+            warehouses.add(sle.get('warehouse'))
+
+    warehouses = sorted(list(warehouses))
+
+    if not warehouses:
+        return [], []
+
+    # نبني structure للـ pivot
+    # {unique_key: {data, warehouses: {wh: balance}}}
+    pivot_structure = {}
+
+    for sle in sl_entries:
+        # نعمل key فريد لكل transaction
+        key = f"{sle.get('date')}_{sle.get('item_code')}_{sle.get('voucher_no')}_{sle.get('voucher_type')}"
+
+        if key not in pivot_structure:
+            item_detail = item_details.get(sle.get('item_code'), {})
+
+            # نحفظ كل البيانات
+            pivot_structure[key] = {
+                'date': sle.get('date'),
+                'item_code': sle.get('item_code'),
+                'item_name': item_detail.get('item_name', ''),
+                'stock_uom': item_detail.get('stock_uom', ''),
+                'in_qty': flt(sle.get('actual_qty', 0) if sle.get('actual_qty', 0) > 0 else 0, 2),
+                'out_qty': flt(sle.get('actual_qty', 0) if sle.get('actual_qty', 0) < 0 else 0, 2),
+                'item_group': item_detail.get('item_group', ''),
+                'brand': item_detail.get('brand', ''),
+                'description': item_detail.get('description', ''),
+                'incoming_rate': flt(sle.get('incoming_rate', 0), 2),
+                'valuation_rate': flt(sle.get('valuation_rate', 0), 2),
+                'stock_value': flt(sle.get('stock_value', 0), 2),
+                'stock_value_difference': flt(sle.get('stock_value_difference', 0), 2),
+                'voucher_type': sle.get('voucher_type'),
+                'voucher_no': sle.get('voucher_no'),
+                'batch_no': sle.get('batch_no'),
+                'serial_no': sle.get('serial_no'),
+                'project': sle.get('project'),
+                'company': sle.get('company'),
+                'warehouses': {}
+            }
+
+        # نضيف الـ balance qty للـ warehouse
+        warehouse = sle.get('warehouse')
+        if warehouse:
+            pivot_structure[key]['warehouses'][warehouse] = flt(
+                sle.get('qty_after_transaction', 0), 2)
+
+    # نبني الأعمدة - كل حاجة زي الـ original
+    pivot_columns = [
+        {"label": _("Date"), "fieldname": "date",
+         "fieldtype": "Datetime", "width": 150},
+        {"label": _("Item"), "fieldname": "item_code",
+         "fieldtype": "Link", "options": "Item", "width": 100},
+        {"label": _("Item Name"), "fieldname": "item_name", "width": 100},
+        {"label": _("Stock UOM"), "fieldname": "stock_uom",
+         "fieldtype": "Link", "options": "UOM", "width": 90},
+    ]
+
+    # نضيف inventory dimensions لو موجودة
+    for dimension in get_inventory_dimensions():
+        pivot_columns.append({
+            "label": _(dimension.doctype),
+            "fieldname": dimension.fieldname,
+            "fieldtype": "Link",
+            "options": dimension.doctype,
+            "width": 110,
+        })
+
+    pivot_columns.extend([
+        {"label": _("In Qty"), "fieldname": "in_qty",
+         "fieldtype": "Float", "width": 80, "convertible": "qty"},
+        {"label": _("Out Qty"), "fieldname": "out_qty",
+         "fieldtype": "Float", "width": 80, "convertible": "qty"},
+    ])
+
+    # نضيف عمود لكل warehouse (هنا الفرق - Balance Qty لكل warehouse)
+    warehouse_fieldnames = {}
+    for warehouse in warehouses:
+        # نعمل fieldname آمن
+        safe_fieldname = "wh_" + "".join(
+            c if c.isalnum() else '_'
+            for c in warehouse
+        )[:60]
+
+        warehouse_fieldnames[warehouse] = safe_fieldname
+
+        pivot_columns.append({
+            "label": f"{warehouse}",  # اسم الـ warehouse كعنوان العمود
+            "fieldname": safe_fieldname,
+            "fieldtype": "Float",
+            "width": 110
+        })
+
+    # باقي الأعمدة زي الـ original
+    pivot_columns.extend([
+        {"label": _("Item Group"), "fieldname": "item_group",
+         "fieldtype": "Link", "options": "Item Group", "width": 100},
+        {"label": _("Brand"), "fieldname": "brand",
+         "fieldtype": "Link", "options": "Brand", "width": 100},
+        {"label": _("Description"), "fieldname": "description", "width": 200},
+        {"label": _("Incoming Rate"), "fieldname": "incoming_rate", "fieldtype": "Currency", "width": 110,
+         "options": "Company:company:default_currency", "convertible": "rate"},
+        {"label": _("Avg Rate (Balance Stock)"), "fieldname": "valuation_rate",
+         "fieldtype": filters.get('valuation_field_type', 'Currency'), "width": 180,
+         "options": "Company:company:default_currency" if filters.get('valuation_field_type') == "Currency" else None,
+         "convertible": "rate"},
+        {"label": _("Balance Value"), "fieldname": "stock_value", "fieldtype": "Currency", "width": 110,
+         "options": "Company:company:default_currency"},
+        {"label": _("Value Change"), "fieldname": "stock_value_difference", "fieldtype": "Currency", "width": 110,
+         "options": "Company:company:default_currency"},
+        {"label": _("Voucher Type"),
+         "fieldname": "voucher_type", "width": 110},
+        {"label": _("Voucher #"), "fieldname": "voucher_no",
+         "fieldtype": "Dynamic Link", "options": "voucher_type", "width": 100},
+        {"label": _("Batch"), "fieldname": "batch_no",
+         "fieldtype": "Link", "options": "Batch", "width": 100},
+        {"label": _("Serial No"), "fieldname": "serial_no",
+         "fieldtype": "Link", "options": "Serial No", "width": 100},
+        {"label": _("Project"), "fieldname": "project",
+         "fieldtype": "Link", "options": "Project", "width": 100},
+        {"label": _("Company"), "fieldname": "company",
+         "fieldtype": "Link", "options": "Company", "width": 110},
+    ])
+
+    # نبني الصفوف
+    pivot_rows = []
+
+    for key in sorted(pivot_structure.keys()):
+        entry = pivot_structure[key]
+
+        row = {
+            'date': entry['date'],
+            'item_code': entry['item_code'],
+            'item_name': entry['item_name'],
+            'stock_uom': entry['stock_uom'],
+            'in_qty': entry['in_qty'],
+            'out_qty': entry['out_qty'],
+            'item_group': entry['item_group'],
+            'brand': entry['brand'],
+            'description': entry['description'],
+            'incoming_rate': entry['incoming_rate'],
+            'valuation_rate': entry['valuation_rate'],
+            'stock_value': entry['stock_value'],
+            'stock_value_difference': entry['stock_value_difference'],
+            'voucher_type': entry['voucher_type'],
+            'voucher_no': entry['voucher_no'],
+            'batch_no': entry['batch_no'],
+            'serial_no': entry['serial_no'],
+            'project': entry['project'],
+            'company': entry['company'],
+        }
+
+        # نضيف الـ balance qty لكل warehouse
+        for warehouse in warehouses:
+            fieldname = warehouse_fieldnames[warehouse]
+            row[fieldname] = entry['warehouses'].get(warehouse, 0)
+
+        pivot_rows.append(row)
+
+    return pivot_columns, pivot_rows
 
 
 def get_segregated_bundle_entries(sle, bundle_details, batch_balance_dict, filters):
@@ -237,21 +432,21 @@ def get_columns(filters):
             {
                 "label": _("In Qty"),
                 "fieldname": "in_qty",
-                "fieldtype": "Int",
+                "fieldtype": "Float",
                 "width": 80,
                 "convertible": "qty",
             },
             {
                 "label": _("Out Qty"),
                 "fieldname": "out_qty",
-                "fieldtype": "Int",
+                "fieldtype": "Float",
                 "width": 80,
                 "convertible": "qty",
             },
             {
                 "label": _("Balance Qty"),
                 "fieldname": "qty_after_transaction",
-                "fieldtype": "Int",
+                "fieldtype": "Float",
                 "width": 100,
                 "convertible": "qty",
             },
