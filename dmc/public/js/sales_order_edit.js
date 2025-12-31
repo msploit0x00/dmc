d376ec338361a0c3e369fc0070d706ceb579a3ab
MinaAshraf67 <minaaashraff670@gmail.com>
2025-12-31 10:23:43 +0200

    Sales order not saved issue

--- dmc/public/js/sales_order_edit.js
+++ dmc/public/js/sales_order_edit.js
@@ -18,7 +18,7 @@ frappe.ui.form.on('Sales Order', {
                 frm.set_df_property('customer', 'read_only', 1);
             }, 400);
         }
-        handle_tax_logic_from_address(frm);
+        // handle_tax_logic_from_address(frm);
     },
 
     after_save(frm) {
